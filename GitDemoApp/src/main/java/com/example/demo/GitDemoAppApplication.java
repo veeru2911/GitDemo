1136879aaf0dc57bca3204c51cb4abd7d85df414
veera <veera@LAPTOP-K170QCDV>
2022-06-14 15:43:55 +0530

    first commit

--- GitDemoApp/src/main/java/com/example/demo/GitDemoAppApplication.java
+++ GitDemoApp/src/main/java/com/example/demo/GitDemoAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemoAppApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		System.out.println("This is my first commit");
 		SpringApplication.run(GitDemoAppApplication.class, args);
 		
 	}
